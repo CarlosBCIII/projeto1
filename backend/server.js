4fdb0e499623ee2501a24da8f4f952add8b48629
CarlosBCIII <88202826+CarlosBCIII@users.noreply.github.com>
2025-01-29 22:51:27 -0300

    config do deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import productsRoutes from "./routes/product.route.js"
 
@@ -7,10 +8,20 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());// permite o uso do json no req.body
 
 app.use("/api/products", productsRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'));
+
+    })
+}
+
 
 app.listen(PORT, () => {
     connectDB();
